34a8adda8dfb634a9d7efd185ad64023183f2457
joab <j.torreys@outlook.com>
2021-09-22 16:34:17 -0400

    made changes

--- dojo/src/App.js
+++ dojo/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          joab torreys bombo clattttttttttttttttt!!!!!!!
         </p>
         <a
           className="App-link"
