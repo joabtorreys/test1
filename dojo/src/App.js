3e4cbe446cd4a34dfcdf25f8581f22ae8a6263c8
joab <j.torreys@outlook.com>
2021-09-26 10:02:21 -0400

    first initial save

--- dojo/src/App.js
+++ dojo/src/App.js
@@ -1,10 +1,28 @@
+import './app.css'
 
+import Home from './components/home'
+
+// after imports
 
 function App() {
   return (
-    <div className="App">
-     <h1>hello world</h1>
-    </div>
+
+  <div className="main">
+ <div id="container">
+
+<div className="div1">hello</div>
+<div className="div2">
+  
+  
+  
+  <a href="#">home</a>
+  <a href="#">home</a>
+  <a href="#">home</a>
+  </div>
+
+ </div>
+ <Home/>
+ </div>
   );
 }
 
