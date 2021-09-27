a08bef9b22ef8ed9827e0e6fa88d9a62febee142
joab <j.torreys@outlook.com>
2021-09-27 09:48:24 -0400

    stedy save point

--- dojo/src/components/header.js
+++ dojo/src/components/header.js
@@ -4,11 +4,15 @@ import "../css/header.css";
 const Header = () => {
   return (
     <div id="container">
-      <div className="div1">hello</div>
+      <div className="div1">
+        
+        <h2 className="logo">WE-CO</h2>
+    <p className="tagline">Landscaping Services</p>
+      </div>
       <div className="div2">
-        <Link to="/">home</Link>
-        <Link to="/about">about</Link>
-        <Link to="/contact">contact</Link>
+        <Link className="links" to="/">home</Link>
+        <Link className="links" to="/about">about</Link>
+        <Link className="links" to="/contact">contact</Link>
       </div>
     </div>
   );
